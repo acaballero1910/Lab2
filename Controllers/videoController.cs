0a2035df16db83ff03d4719bbd17e03149af55fa
Andrea Iridian Caballero <acaballero@gmail.com>
2020-03-02 11:04:22 -0600

    primer avance

---
+++ Controllers/videoController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Data.SqlClient;
+using System.Data;
+using MVCLaboratorio.Utilerias;
+
+
+namespace MVCLaboratorio.Controllers
+{
+    public class videoController : Controller
+    {
+        //
+        // GET: /video/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(int idVideo,
+                                    string titulo,
+                                    int repro,
+                                    string url)
+        {
+            //guardar el video
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@idVideo", idVideo));
+            parametros.Add(new SqlParameter("@titulo", titulo));
+            parametros.Add(new SqlParameter("@repro", repro));
+            parametros.Add(new SqlParameter("@url", url));
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete(int idVideo)
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(int idVideo,
+                                    string titulo,
+                                    int repro,
+                                    string url)
+        {
+            return View();
+        }
+
+    }
+}
